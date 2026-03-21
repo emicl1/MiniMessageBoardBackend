87ee262bdf43e6d39834f9c5b54f51a8b8c0a3e0
Alex Olivier Michaud <michaal4@fel.cvut.cz>
2026-03-21 16:17:57 +0100

    :ambulance: repair app

--- app.js
+++ app.js
@@ -1,19 +1,10 @@
 const express = require("express");
 const indexRoutes = require("./routes/index")
 const newRoutes = require("./routes/new.js")
-const html = require("html")
 
 const app = express();
 
 app.set('view engine', 'ejs')
-
-const PORT = 3000;
-app.listen(PORT, (error) => {
-    if (error) {
-        throw error;
-    }
-    console.log(`My first Express app - listening on port ${PORT}!`);
-});
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
@@ -26,5 +17,13 @@ app.use(express.static('public'));
 app.use("/", indexRoutes)
 app.use("/new", newRoutes)
 
+
+const PORT = 3000;
+app.listen(PORT, (error) => {
+    if (error) {
+        throw error;
+    }
+    console.log(`My first Express app - listening on port ${PORT}!`);
+});
 
 
